69be71201f3068e31b38a3ffbbb8c2c32a2051e5
Sadia Tasnim <tasnimsadia259@gmail.com>
2023-06-11 14:02:05 +0600

    Corner Radius added

--- UserCoreDataProject2/ViewController.swift
+++ UserCoreDataProject2/ViewController.swift
@@ -8,7 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,6 +20,36 @@ class ViewController: UIViewController {
 
     @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
         
+        let registerViewController = self.storyboard?.instantiateViewController(withIdentifier: "RegisterViewController") as! RegisterViewController
+                
+                self.navigationController?.pushViewController(registerViewController, animated: true)
     }
+
 }
 
+
+extension ViewController: UISearchBarDelegate {
+    //MARK:- SEARCH BAR DELEGATE METHOD FUNCTION
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        
+        //users = databaseManager.fetchUser()
+        
+        searchBar.endEditing(true)
+        tableView.reloadData()
+    }
+    
+    
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty{
+            //users = databaseManager.fetchUser()
+        }else{
+            //users = databaseManager.fetchUser(keyword: searchText)
+        }
+        
+        tableView.reloadData()
+    }
+    
+}
